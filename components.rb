8b89a31391b01cd7c4509262b70f148d1aa360dd
Jason Rogers <jrogers@hedgeye.com>
2024-06-03 17:19:44 -0400

    fix: remove unnecessary "next" in auto-require

--- components.rb
+++ components.rb
@@ -9,14 +9,11 @@ require_relative 'components/rendered_results_preview_component'
 require_relative 'components/phlex_preview_app'
 require_relative 'components/component_selector'
 require_relative 'components/validateable_input'
-
 require_relative 'components/labelized_checkbox'
 require_relative 'components/labelized_input'
 require_relative 'components/labelized_magnitude_input'
 
 relative_requirer = ->(f) { require_relative File.join('components', File.basename(f, '.rb')) }
 Dir.glob(File.join(__dir__, 'components', '*.rb')).each do |f|
-  next if f =~ /labelized_checkbox|labelized_input|labelized_magnitude_input/
-
   require_relative File.join('components', File.basename(f, '.rb'))
 end
