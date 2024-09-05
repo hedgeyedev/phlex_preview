b78b610bcd86f7c0dc841bda00a17551c7267cb6
Jason Rogers <jrogers@hedgeye.com>
2024-09-05 09:44:36 -0400

    fix: removes built assets directory

--- app/assets/config/phlex_storybook_manifest.js
+++ app/assets/config/phlex_storybook_manifest.js
@@ -1,4 +1,3 @@
-//= link_tree ../builds/ .css
 //= link_tree ../images/phlex_storybook .svg
 //= link_directory ../../javascript/phlex_storybook .js
 //= link_directory ../../javascript/phlex_storybook/controllers .js
