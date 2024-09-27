240a44ce2a948f84e7eb2183ac2194d6d751a966
Scott Deming <sdeming@hedgeye.com>
2024-09-27 17:52:24 -0400

    [#188347131] chore: re-add missing preload: true

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,10 @@
 pin 'monaco-editor', to: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.51.0/+esm'
 
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
-pin "phlex_storybook", to: "phlex_storybook/application.js"
+pin "phlex_storybook", to: "phlex_storybook/application.js", preload: true
 pin_all_from PhlexStorybook::Engine.root.join("app/javascript/phlex_storybook/controllers"),
              under: "controllers",
              to: "phlex_storybook/controllers",
